6f9a9a37bf780917c6b1e14b3adaaec365ebdce4
victorm27 <victormanuel272791@gmail.com>
2023-09-15 22:59:27 -0500

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Pokemon, Pokemons  } from './api/pages';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="app">
+     <Routes>
+      <Route path="/pokemons" element={<Pokemon />} />
+      <Route path="/pokemons/:name" element={<Pokemon />} />
+      <Route path="/" element={<Pokemons />} />
+     </Routes>
     </div>
+    </Router>
   );
 }
 
